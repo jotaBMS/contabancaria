6b0cad6a7059086864b807ed6ce2baf408b3fc6e
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-11-07 00:50:56 -0300

    atividade Java 11

--- conta/Menu.java
+++ conta/Menu.java
@@ -1,10 +1,21 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
+import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		//teste de classe Conta
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		
 		Scanner scanner = new Scanner(System.in);
 		
