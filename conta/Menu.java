248013887cdf2f705b6a371c5662ab8de1eee079
João Bispo <joaovitor.bispo.1000@gmail.com>
2024-11-11 17:14:44 -0300

    task 4 e 5

--- conta/Menu.java
+++ conta/Menu.java
@@ -3,24 +3,41 @@ package conta;
 import java.util.Scanner;
 import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		
-		//teste de classe Conta
+
+		/*// teste de classe Conta
 		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
 		c1.visualizar();
 		c1.sacar(12000.0f);
 		c1.visualizar();
 		c1.depositar(5000.0f);
-		c1.visualizar();
-		
-		
+		c1.visualizar();*/
+
+		// Teste da Classe Conta Corrente
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+
+		// Teste da Classe Conta Poupança
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+
 		Scanner scanner = new Scanner(System.in);
-		
+
 		int opcao;
-		
+
 		while (true) {
 
 			System.out.println("*****************************************************");
@@ -44,61 +61,59 @@ public class Menu {
 			System.out.println("                                                     ");
 
 			opcao = scanner.nextInt();
-			
+
 			if (opcao == 9) {
 				System.out.println("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
 				sobre();
-                 scanner.close();
+				scanner.close();
 				System.exit(0);
 			}
 
 			switch (opcao) {
-				case 1:
-					System.out.println("Criar Conta\n\n");
+			case 1:
+				System.out.println("Criar Conta\n\n");
 
-					break;
-				case 2:
-					System.out.println("Listar todas as Contas\n\n");
+				break;
+			case 2:
+				System.out.println("Listar todas as Contas\n\n");
 
-					break;
-				case 3:
-					System.out.println("Consultar dados da Conta - por número\n\n");
+				break;
+			case 3:
+				System.out.println("Consultar dados da Conta - por número\n\n");
 
-					break;
-				case 4:
-					System.out.println("Atualizar dados da Conta\n\n");
+				break;
+			case 4:
+				System.out.println("Atualizar dados da Conta\n\n");
 
-					break;
-				case 5:
-					System.out.println("Apagar a Conta\n\n");
+				break;
+			case 5:
+				System.out.println("Apagar a Conta\n\n");
 
-					break;
-				case 6:
-					System.out.println("Saque\n\n");
+				break;
+			case 6:
+				System.out.println("Saque\n\n");
 
-					break;
-				case 7:
-					System.out.println("Depósito\n\n");
+				break;
+			case 7:
+				System.out.println("Depósito\n\n");
 
-					break;
-				case 8:
-					System.out.println("Transferência entre Contas\n\n");
+				break;
+			case 8:
+				System.out.println("Transferência entre Contas\n\n");
 
-					break;
-				default:
-					System.out.println("\nOpção Inválida!\n");
-					break;
+				break;
+			default:
+				System.out.println("\nOpção Inválida!\n");
+				break;
 			}
 		}
 	}
-    
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
 		System.out.println("Joao Vitor Bispo - joaovitor.bispo.1000@gmail.com");
 		System.out.println("https://github.com/jotaBMS");
 		System.out.println("*********************************************************");
-		}
 	}
-
-
+}
